307eeb6e036f21d1849c4e6715c5e230a3fd4307
fajrulaslim <fajrul.aslim@gmail.com>
2022-10-14 17:36:12 +0700

    learn

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,14 +1,43 @@
 import Head from "next/head"
 import Layout from "../../components/Layout"
 
-function Blogs() {
+interface Post {
+  id: number,
+  title: string,
+  body: string,
+}
+
+interface BlogProps {
+  dataBlog: Post[]
+}
+
+function Blogs({ dataBlog }: BlogProps) {
   return (
     <>
     <Layout pageTitle="Blog">
-        <h1>Blog</h1>
+        <h1 className="mb-4">Blog</h1>
+        {
+          dataBlog.map((blog) => {
+            return (
+              <div key={blog.id} className="mb-3">
+                <h3>{blog.title}</h3>
+                <p>{blog.body}</p>
+              </div>
+            )
+          })
+        }
       </Layout>
     </>
   )
 }
 export default Blogs
 
+export async function getServerSideProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const dataBlog = await res.json()
+  return {
+    props: {
+      dataBlog
+    }
+  }
+}
